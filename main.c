a2834cce3cf15f7234a1647ee93cc0fb054cbcb7
Armandfm11 <armandfm11@gmail.com>
2025-07-24 14:55:51 +0300

    Clean-up

--- main.c
+++ main.c
@@ -1,24 +1,22 @@
- // 6.3 (a) - "Typedef" predefinite in locul tipurilor numerice de baza (ex.: float64_t in loc de double)
-
-#include <stdio.h>  // 20.9 (r) - Libraria <stdio.h> nu trebuie folosita in cod productie
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
 
-        // Aproximarea unei integrale folosind metoda trapezelor        //     2.2 (r) - Comentarii doar cu / * ... * /
+        // Aproximarea unei integrale folosind metoda trapezelor
 
 
-    // Functia integralei       //     2.2 (r) - Comentarii doar cu / * ... * /
+    // Functia integralei
 
-double f(double x)    //     8.10 (r) - Functiile apelate strict intern trebuie declarate ca "static"
+double f(double x)
 {
     return(1/(x+1));
 }
 
 
-    // Functie de verificare input      //     2.2 (r) - Comentarii doar cu / * ... * /
+    // Functie de verificare input
 
-double Validare(const char *prompt)    //     8.10 (r) - Functiile apelate strict intern trebuie declarate ca "static"
+double Validare(const char *prompt)
 {
     double var;
     char input[100];
@@ -32,12 +30,12 @@ double Validare(const char *prompt)    //     8.10 (r) - Functiile apelate stric
         if (sscanf(input, "%lf", &var) == 1)
         {
             esteValid = true;
-            /* break; */    // 2.4 (a) - Codul nu ar trebui comentat
+            /* break; */
         }
         else
         {
             printf("Input invalid. Te rog sa introduci un numar.\n\n");
-            /* break; */    // 14.6 (r) - Orice loop "if" poate avea maxim un break
+            /* break; */
         }
     } while (!esteValid);
     printf("\n");
@@ -45,37 +43,36 @@ double Validare(const char *prompt)    //     8.10 (r) - Functiile apelate stric
 }
 
 
-int main()      //     16.5 (r) - Functiile fara parametrii trebuie declarate si definite ca "void"
+int main()
 {
-    // Input        //     2.2 (r) - Comentarii doar cu / * ... * /
+    // Input
 
     double a, b, e;
     a = Validare("Introduceti limita inferioara a intervalului: ");
     b = Validare("Introduceti limita superioara a intervalului: ");
     e = Validare("Introduceti precizia: ");
 
-    // Initializare     //     2.2 (r) - Comentarii doar cu / * ... * /
+    // Initializare
     double n = 1;
 
-    // Aproximare initiala - 1 interval     //     2.2 (r) - Comentarii doar cu / * ... * /
+    // Aproximare initiala - 1 interval
     double II, IO, h, s, i;
     II = ((f(a)+f(b)) * (b-a))/2 ;
 
-    // Rafinare aproximare      //     2.2 (r) - Comentarii doar cu / * ... * /
+    // Rafinare aproximare
     do
     {
         n  = 2*n;
         h = (b-a)/n ;
         IO = II;
         s = 0;
-        for(i=1; i<=n-1; i++)       //     13.4 (r) - Variabilele float nu se folosesc in cicluri "for"
+        for(i=1; i<=n-1; i++)
         {
-            s = s + f(a+i*h);       //     12.1 (a) - Parantezele ar trebui folosite pentru ordinea efectuarii operatiilor 
+            s = s + f(a+i*h);
         }
-        II = ( f(a) + 2*s + f(b) ) * h/2 ;      //     12.1 (a) - Parantezele ar trebui folosite pentru ordinea efectuarii operatiilor 
+        II = ( f(a) + 2*s + f(b) ) * h/2 ;
     }while( fabs(II-IO) > e );
 
-    // Output       //     2.2 (r) - Comentarii doar cu / * ... * /
-    printf("Valoarea integralei, obtinuta cu precizia %f este %f", e, II);
-
-}       //     16.8 (r) - Toate functiile non-void trebuie sa aiba un "return" specificat
+    // Output
+        printf("Valoarea integralei, obtinuta cu precizia %f este %f", e, II);
+    }
